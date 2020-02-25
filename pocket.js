95a64e9763be00c8396bf6016596f43f060dca71
Rares <rares_george97@yahoo.com>
2020-02-25 16:47:58 +0200

    added products on the other pages; improved the navigation bar

--- pocket.js
+++ pocket.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:3000/api/socks", {
+fetch("http://localhost:3000/api/pocket", {
   headers: { Authorization: `Bearer ${sessionStorage.getItem("token")}` }
 })
   .then(r => r.json())
