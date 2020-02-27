52c7a9d1f49489df3b67a5c5973162d20a4eab78
Rares <rares_george97@yahoo.com>
2020-02-27 19:07:33 +0200

    added product page

--- ties.js
+++ ties.js
@@ -6,42 +6,46 @@ fetch("http://localhost:3000/api/ties", {
     const mainContainer = document.getElementById("mainContainer");
     for (var i = 0; i < content.length; i++) {
       const card = mainContainer.appendChild(document.createElement("div"));
-      card.classList = "products";
-      const image = card.appendChild(document.createElement("img"));
-      image.setAttribute("src", content[i].image);
-      image.classList = "images";
-      const productName = card.appendChild(document.createElement("h3"));
-      productName.innerHTML = content[i].name;
-      const productPrice = card.appendChild(document.createElement("h4"));
-      productPrice.innerHTML = content[i].price;
+      const productPageLink = card.appendChild(document.createElement("a"));
+      const image = productPageLink.appendChild(document.createElement("img"));
+      const productName = productPageLink.appendChild(document.createElement("h3"));
+      const productPrice = productPageLink.appendChild(document.createElement("h4"));
       const addToCart = card.appendChild(document.createElement("button"));
-      addToCart.classList = "addToCartBtn";
-      addToCart.innerHTML = "Add To Cart";
       const addToWishlist = card.appendChild(document.createElement("button"));
+      productPageLink.setAttribute("href", "productPage.html");
+      image.setAttribute("src", content[i].image);
+      card.classList = "products";
+      image.classList = "images";
+      productName.classList = "productName";
+      productPrice.classList = "productPrice";
+      addToCart.classList = "addToCartBtn";
       addToWishlist.classList = "addToWishlistBtn";
+      productName.innerHTML = content[i].name; 
+      productPrice.innerHTML = content[i].price;
+      addToCart.innerHTML = "Add To Cart";
       addToWishlist.innerHTML = "Add To Wishlist";
     }
   })
   .catch(err => {
-    const errorContainer = document.createElement("div")
-    const errorTitle = document.createElement("h1")
-    const errorDescription = document.createElement("p")
-    errorContainer.appendChild(errorTitle)
-    errorContainer.appendChild(errorDescription)
-    errorContainer.classList = "errorContainer"
-    errorTitle.classList = "errorTitle"
-    errorDescription.classList = "errorDescription"
-    errorTitle.innerHTML = "403"
-    errorDescription.innerHTML = "This is a forbidden area"
-    const footer = document.getElementById("footer")
-    footer.parentNode.insertBefore(errorContainer, footer)
+    const errorContainer = document.createElement("div");
+    const errorTitle = document.createElement("h1");
+    const errorDescription = document.createElement("p");
+    const footer = document.getElementById("footer");
+    errorContainer.appendChild(errorTitle);
+    errorContainer.appendChild(errorDescription);
+    footer.parentNode.insertBefore(errorContainer, footer);
+    errorContainer.classList = "errorContainer";
+    errorTitle.classList = "errorTitle";
+    errorDescription.classList = "errorDescription";
+    errorTitle.innerHTML = "403";
+    errorDescription.innerHTML = "This is a forbidden area";
   })
 
 
 const toTopBtn = document.getElementById("goTopBtn");
 
 window.addEventListener("scroll", scrollFunction);
-toTopBtn.addEventListener("click", backToTop)
+toTopBtn.addEventListener("click", backToTop);
 
 function scrollFunction() {
   if (window.pageYOffset > 200) {
