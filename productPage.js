e30cdf383f19adec6b88923304f6bc13d2d5cb38
Rares <rares_george97@yahoo.com>
2020-03-02 00:51:00 +0200

    improved the product page

--- productPage.js
+++ productPage.js
@@ -36,10 +36,12 @@ function loadProduct() {
     const firstImage = document.getElementById("firstImage")
     const secondImage = document.getElementById("secondImage")
     const mainInformation = document.getElementById("mainInformation")
+    const productDetailsContainer = document.getElementById("productDetailsContainer")
     firstImage.innerHTML = `<img src="${product.image}" alt="" style="width: 100%; height:100%";/>`
     secondImage.innerHTML = `<img src="${product.image2}" alt="" style="width: 100%; height:100%"/>`
     imageCarousel.innerHTML = `<img src="${product.image2}" id="lastClone" alt="" style="width: 400px" /> <img src="${product.image}" alt="" style="width: 400px"/>  <img src="${product.image2}" alt="" style="width: 400px"/> <img src="${product.image}" id="firstClone" alt="" style="width: 400px"/>`
-    mainInformation.innerHTML = `<h1>${product.name}</h1> <h5>${product.info.brand}® by Tiesnsocks.com</h5><h2>${product.price}</h2><p>FREE SHIPPING & RETURNS*</p><button id="addToCart">ADD TO CART</button><button id="addToWishlist">ADD TO WISHLIST</button><div id="qty"><button id="remove">-</button><span><p>Quantity: </p><p>1</p></span><button id="add">+</button></div><p id="description">${product.description}</p>`
+    mainInformation.innerHTML = `<div id="namePrice"><h1>${product.name}</h1> <h5>${product.info.brand}® by Tiesnsocks.com</h5><h2>${product.price}</h2><p>FREE SHIPPING & RETURNS*</p></div><div id="buttons"><button id="addToCart">ADD TO CART</button><button id="addToWishlist">ADD TO WISHLIST</button><div id="qty"><button id="remove">-</button><span><p>Quantity: </p><p>1</p></span><button id="add">+</button></div></div><p id="description">${product.description}</p>`
+    productDetailsContainer.innerHTML = `<hr><h2 id="title">About the product</h2><div id="productDetails"><p><b>Name: </b>${product.name}</p><p><b>Color: </b>${product.info.color}</p><p><b>Brand: </b>${product.info.brand}</p><p><b>Material: </b>${product.info.material}</p><p><b>Size: </b>${product.info.size}</p></div>`
     console.log(product)
 }
 
